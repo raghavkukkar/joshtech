1120b0c950a6f2b054cafd17882d73fe736ca050
raghavkukkar <raghavkukkar9@gmail.com>
2020-11-29 14:28:21 +0530

    content container slider and two ads

---
+++ raghav/main.js
@@ -0,0 +1,10 @@
+let slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    navPosition: 'bottom',
+    controls: false,
+    autoplayButtonOutput: false,
+    navContainer : '#control-buttons-container'
+});
